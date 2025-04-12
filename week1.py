4006a8dce47a2ff2cb062d16ebe3fd9687359803
cindy-prog <cindynjeri37@gmail.com>
2025-04-12 22:31:14 +0300

    added a new file

---
+++ week1.py
@@ -0,0 +1,16 @@
+a= int (input("enter the value of a:"))
+b= int (input("enter the value of b:"))
+
+sum = a+b 
+print("the sum is:", sum)
+subtraction =a-b
+print("the subtraction is:", subtraction)
+multiplication = a*b
+print("the multiplication is:", multiplication)
+division = a/b 
+print("the division is:", division)
+
+
+
+
+
